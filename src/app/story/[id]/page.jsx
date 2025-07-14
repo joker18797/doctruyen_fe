c526da0949f5d9d0ee0ad460bad44b317d882266
danh <danh.bq@m2m-sol.co.jp>
2025-07-14 23:28:28 +0700

    update

--- src/app/story/[id]/page.jsx
+++ src/app/story/[id]/page.jsx
@@ -4,16 +4,27 @@ import API from '@/Service/API'
 import StoryInfoPage from '@/components/StoryInfoPage' // move UI vào component riêng
 
 export async function generateMetadata({ params }) {
-  const story = await API.Story.detail(params.id)
+  const story = await API.Story.detail(params?.id)
   const s = story.data
 
   return {
     title: s?.title,
     description: s?.description?.slice(0, 150),
     openGraph: {
+      type: 'article', // hoặc 'website' nếu không phải bài viết
+      url: `https://ocuadua.com/story/${params?.id}`,
       title: s?.title,
       description: s?.description?.slice(0, 150),
-      images: [s?.coverImage],
+      siteName: 'Tên website của bạn',
+      images: [
+        {
+          url: s?.coverImage,
+          width: 1200,
+          height: 630,
+          alt: s?.title,
+        },
+      ],
+      locale: 'vi_VN', // hoặc 'en_US'
     },
     twitter: {
       card: 'summary_large_image',
