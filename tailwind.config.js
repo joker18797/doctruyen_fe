f599976f360d67a6c7bdf6ee14542b83f6b44cc6
danh <danh.bq@m2m-sol.co.jp>
2025-07-14 23:42:31 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rounded: ['"M PLUS Rounded 1c"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
