500ee2634a9afae5046806172c0f0414e04a6cdf
danhbq <danh.bq@taki.vn>
2026-03-26 08:28:04 +0700

    fix bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.jsdelivr.net", "res.cloudinary.com"], 
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.jsdelivr.net",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ],
   },
 };
 export default nextConfig;
